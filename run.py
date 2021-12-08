e5271df848bf22b2e7bb1fabd18a97e29d5527ca
Nicolas Torneri <nicolas.torneri@gmail.com>
2021-12-08 23:10:07 +0100

    update implementation to match davidar

--- run.py
+++ run.py
@@ -4,15 +4,15 @@ import sys
 import msvcrt
 with open(sys.argv[1]) as f:
     ram = [int(s) for line in f for s in line.split()]
-ip = 0
-while ip >= 0:
-    a, b, c = ram[ip:ip+3]
+pc = 0
+while pc >= 0:
+    a, b, c = ram[pc:pc+3]
+    pc += 3
     if a < 0:
-        x = ord(msvcrt.getch())
+        ram[b] += ord(msvcrt.getch())
+    elif b < 0:
+        print(chr(ram[a]), end='')
     else:
-        x = ram[a]
-    if b < 0:
-        print(chr(x), end='')
-    else:
-        ram[b] -= x
-    ip = c if b >= 0 and ram[b] <= 0 else ip+3
+        ram[b] -= ram[a]
+        if ram[b] <= 0:
+            pc = c
